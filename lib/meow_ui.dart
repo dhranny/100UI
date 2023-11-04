c5f9b917e45ea784da574784b7caf40d06e53329
name <dhrannydan2018@gmail.com>
2023-11-04 11:19:13 +0100

    Done with meow form page

--- lib/meow_ui.dart
+++ lib/meow_ui.dart
@@ -14,78 +14,117 @@ class MeowUiState extends State<MeowUi>{
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: [
-          Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Container(
-                decoration: BoxDecoration(
-                  image: const DecorationImage(
-                    image: AssetImage('images/bg.png'),
-                    fit: BoxFit.fill
-                  ),
-                  borderRadius: BorderRadius.all(Radius.circular(20))
-                ),
-                child: SvgPicture.asset('images/pawnadoption.svg',),
+      body: Container(
+        decoration: BoxDecoration(
+          color: Colors.black12
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Container(
+              decoration: BoxDecoration(
+                color: Colors.white
               ),
-              const Text('Meow!',
-                textAlign: TextAlign.center,
-                style: TextStyle(fontFamily: 'Inter', fontSize: 30, fontWeight: FontWeight.w600),
-              ),
-              Text('Welcome to our adoption app!\nI hope you will find what you are looking for',
-                textAlign: TextAlign.center,
-                style: TextStyle(fontFamily: 'Inter', fontSize: 16),
-              ),
-              TextField(
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                  hintText: 'Joe Doe',
-                ),
-                keyboardType: TextInputType.name,
-                maxLines: 1,
+              child: Padding(
+                padding: EdgeInsets.only(left: 20, right: 20, top: 100, bottom: 100),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Container(
+                      decoration: BoxDecoration(
+                          image: const DecorationImage(
+                              image: AssetImage('images/bg.png'),
+                              fit: BoxFit.fill
+                          ),
+                          borderRadius: BorderRadius.all(Radius.circular(20))
+                      ),
+                      padding: EdgeInsets.all(5),
+                      child: SvgPicture.asset('images/pawnadoption.svg',height: 60, width: 60,),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.only(top: 30),
+                      child: const Text('Meow!',
+                        textAlign: TextAlign.center,
+                        style: TextStyle(fontFamily: 'Inter', fontSize: 30, fontWeight: FontWeight.w600),
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.only(top: 5),
+                      child: Text('Welcome to our adoption app!\nI hope you will find what you are looking for!',
+                        textAlign: TextAlign.center,
+                        style: TextStyle(fontFamily: 'Inter', fontSize: 16, fontWeight: FontWeight.w500),
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.only(top: 30, bottom: 10),
+                      child: TextField(
+                        decoration: InputDecoration(
+                          border: OutlineInputBorder(borderRadius: BorderRadius.all(Radius.circular(30))),
+                          hintText: 'Joe Doe',
+                        ),
+                        keyboardType: TextInputType.name,
+                        maxLines: 1,
 
-              ),
-              TextField(
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                  hintText: 'joe.doe@gmail.com',
-                ),
-                keyboardType: TextInputType.emailAddress,
-                maxLines: 1,
+                      ),
+                    ),
+                    TextField(
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(borderRadius: BorderRadius.all(Radius.circular(30))),
+                        hintText: 'joe.doe@gmail.com',
+                      ),
+                      keyboardType: TextInputType.emailAddress,
+                      maxLines: 1,
 
-              ),
-              TextField(
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                  hintText: '***********',
-                  suffixIcon: Icon(Icons.visibility, color: Theme.of(context).primaryColor,)
-                ),
-                obscureText: true,
-                enableSuggestions: false,
-                autocorrect: false,
-                maxLines: 1,
-              ),
-              ElevatedButton(
-                onPressed: (){},
-                child: Text('Sign In'),
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: Colors.red
-                ),
-              ),
-              Row(
-                children: [
-                  Icon(Icons.facebook_outlined)
-                ]
-              )
-            ],
-          ),
-          Row(
+                    ),
+                    Padding(
+                      padding: EdgeInsets.only(top: 10, bottom: 10),
+                      child: TextField(
+                        decoration: InputDecoration(
+                            border: OutlineInputBorder(borderRadius: BorderRadius.all(Radius.circular(30))),
+                            hintText: '***********',
+                            suffixIcon: Icon(Icons.visibility, color: Theme.of(context).primaryColor,)
+                        ),
+                        obscureText: true,
+                        enableSuggestions: false,
+                        autocorrect: false,
+                        maxLines: 1,
+                      ),
+                    ),
+                    ElevatedButton(
+                      onPressed: (){},
+                      child: Text('Sign In'),
 
-          )
-        ],
-      ),
+                      style: ElevatedButton.styleFrom(
+                          backgroundColor: Colors.red,
+                          minimumSize: Size(double.infinity, 60)
+                      ),
+                    ),
+                    Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Icon(Icons.facebook_outlined),
+                          Text('Sign Up with facebook', style: TextStyle(fontFamily: 'Inter', fontWeight: FontWeight.w500),)
+                        ]
+                    )
+                  ],
+                ),
+              ),
+            ),
+            SizedBox(
+                height: 100,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text('Have an account?', style: TextStyle(fontFamily: 'Inter', fontWeight: FontWeight.w500)),
+                    GestureDetector(onTap: (){},
+                        child: Text('Log in', style: TextStyle(fontFamily: 'Inter', fontWeight: FontWeight.w500, color: Colors.red))
+                    )
+                  ],
+                )
+            )
+          ],
+        ),
+      )
     );
   }
 
